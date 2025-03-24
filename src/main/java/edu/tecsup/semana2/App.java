7ff09389d8a585a168dbe3b9e62fd5c3ad9df3d6
Luis Carrion Castañeda <luiscastaneda019@gmail.com>
2025-03-24 16:07:54 -0500

    change saludation message

--- src/main/java/edu/tecsup/semana2/App.java
+++ src/main/java/edu/tecsup/semana2/App.java
@@ -5,6 +5,6 @@ package edu.tecsup.semana2;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Salut les gars!");
     }
 }
